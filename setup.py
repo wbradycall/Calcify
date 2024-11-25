62e20bdebbe8bae24417910e05721dba0ce7c31e
wbradycall <151959613+wbradycall@users.noreply.github.com>
2024-11-25 10:15:57 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='Calcify',
+    version='0.0.1',
+    description="A calculus library for python",
+    author="Brady Call",
+    author_email="wbradycall@gmail.com",
+    packages=['calcify'],
+    intall_requires=['numpy']
+)
